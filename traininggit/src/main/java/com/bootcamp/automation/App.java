62b56fcd93121299d77f8bc89cdb11555e4b30fc
Alex Cumbe <alexander.cumbe@gmail.com>
2021-04-05 19:59:00 -0500

    created sum and subs methods

--- traininggit/src/main/java/com/bootcamp/automation/App.java
+++ traininggit/src/main/java/com/bootcamp/automation/App.java
@@ -8,6 +8,20 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        double suma =  sumar(3,3);
+        double restar =  restar(15,5);
+        System.out.println("La suma es:" + suma);
+        System.out.println("La resta es" + restar);
     }
+
+    public static double sumar(double num1, double num2){
+        double suma = num1+num2;
+        return suma;
+    }
+
+    public static double restar(double num1, double num2){
+        double resta = num1-num2;
+        return resta;
+    }
+
 }
