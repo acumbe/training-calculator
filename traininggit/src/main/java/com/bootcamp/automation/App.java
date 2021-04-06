03890c5caf119ca1c5164891d93f4170322faeab
Alex Cumbe <alexander.cumbe@gmail.com>
2021-04-05 20:07:54 -0500

    created multiplication and division methods

--- traininggit/src/main/java/com/bootcamp/automation/App.java
+++ traininggit/src/main/java/com/bootcamp/automation/App.java
@@ -10,8 +10,12 @@ public class App
     {
         double suma =  sumar(3,3);
         double restar =  restar(15,5);
+        double multiplica =  multiplication(4,5);
+        double divide =  division(15,5);
         System.out.println("La suma es:" + suma);
         System.out.println("La resta es" + restar);
+        System.out.println("La multiplicacion es: " + multiplica);
+        System.out.println("La divicion es: " + divide);
     }
 
     public static double sumar(double num1, double num2){
@@ -22,6 +26,16 @@ public class App
     public static double restar(double num1, double num2){
         double resta = num1-num2;
         return resta;
+    }
+
+    public static double multiplication(double num1, double num2){
+        double mul = num1*num2;
+        return mul;
+    }
+
+    public static double division(double num1, double num2){
+        double div = num1/num2;
+        return div;
     }
 
 }
